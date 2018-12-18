015d3f0a1a1116d1c3661ece5bf10d88c0949e04
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-12-18 16:16:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -32,7 +32,7 @@ client.on('message', message => {
      
       const banembed = new Discord.RichEmbed()
       .setAuthor('Kicked !', user.displayAvatarURL)
-      .setColor("RANDOM")
+            .setColor('#000000').setColor('#36393e')
       .setTimestamp()
       .addField("User:",  `[ + ${user.tag} + ]`)
       .addField("By:", `[  + ${message.author.tag} +  ]`)
@@ -50,7 +50,7 @@ client.on('message', message => {
             message.guild.owner.send(`لقد تم فك الباند عن الشخص \n ${user} \n By : <@${message.author.id}>`)
             var embed = new Discord.RichEmbed()
             .setThumbnail(message.author.avatarURl)
-            .setColor("RANDOM")
+            .setColor('#000000').setColor('#36393e')
             .setTitle('**●Unban** !')
             .addField('**●User Unban :** ', `${user}` , true)
             .addField('**●By :**' ,       ` <@${message.author.id}> ` , true)
@@ -326,5 +326,64 @@ client.on('message', message => {
        client.on ("guildMemberRemove", member => {
           
        })
+
+client.on("message", (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.toLowerCase().startsWith(prefix + `ping`)) {
+    message.channel.send(`Hoold on!`).then(m => {
+    m.edit(`:ping_pong: Wew, made it over the ~waves~ ! **Pong!**\nMessage edit time is ` + (m.createdTimestamp - message.createdTimestamp) + `ms, Discord API heartbeat is ` + Math.round(client.ping) + `ms.`);
+    });
+}
+
+if (message.content.toLowerCase().startsWith(prefix + `new`)) {
+    const reason = message.content.split(" ").slice(1).join(" ");
+    if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
+        let role = message.guild.roles.find("name", "Support Team");
+        let role2 = message.guild.roles.find("name", "@everyone");
+        c.overwritePermissions(role, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        c.overwritePermissions(role2, {
+            SEND_MESSAGES: false,
+            READ_MESSAGES: false
+        });
+        c.overwritePermissions(message.author, {
+            SEND_MESSAGES: true,
+            READ_MESSAGES: true
+        });
+        message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
+        const embed = new Discord.RichEmbed()
+            .setColor('#000000').setColor('#36393e')
+        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
+        .setTimestamp();
+        c.send({ embed: embed });
+    }).catch(console.error);
+}
+if (message.content.toLowerCase().startsWith(prefix + `close`)) {
+    if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
+
+    message.channel.send(`Are you sure? Once confirmed, you cannot reverse this action!\nTo confirm, type \`-confirm\`. This will time out in 10 seconds and be cancelled.`)
+    .then((m) => {
+      message.channel.awaitMessages(response => response.content === '-confirm', {
+        max: 1,
+        time: 10000,
+        errors: ['time'],
+      })
+      .then((collected) => {
+          message.channel.delete();
+        })
+        .catch(() => {
+          m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
+              m2.delete();
+          }, 3000);
+        });
+    });
+}
+
+});
 
 client.login(process.env.BOT_TOKEN);
