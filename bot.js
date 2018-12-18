f746111f2cf004038498be55a45c92ffd93ff441
XKillerYT <43609934+XKillerYT@users.noreply.github.com>
2018-12-18 16:32:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const moment = require("moment"),
-const prefix = "=";
+const moment = require("moment");
+const prefix = "="
 const devs = ['461386341146558475'];
 
 
